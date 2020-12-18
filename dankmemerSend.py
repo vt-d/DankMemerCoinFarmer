8ae1b1acd9f896103f7b15beca5430aa799d0baf
Simple <69445064+Simple2006@users.noreply.github.com>
2020-12-18 14:46:28 -0800

    Update dankmemerSend.py

--- dankmemerSend.py
+++ dankmemerSend.py
@@ -4,7 +4,7 @@ class MyClient(discord.Client):
 
     async def on_ready(self):
         print("Logged on as {0}!".format(self.user))
-        channel = client.get_channel(751276567463854120)
+        channel = client.get_channel(ENTER CHANNEL ID)
         while True:
             await channel.send("pls hunt")
             time.sleep(5)
@@ -24,5 +24,4 @@ class MyClient(discord.Client):
             time.sleep(15)
             
 client = MyClient()
-client.run("Mzc5NDcwMTg4NjY5ODI5MTIx.X6loFQ.8-TAOVT3HD7Wr0NyExRYK_wVXXw", bot = False)
-#NzEzNTY3NjEyOTk0NDUzNTA0.X9qB6g.DVYlwTZhNbZsjgn0zmqPdNmaebY
+client.run("ENTER TOKEN", bot = False)
