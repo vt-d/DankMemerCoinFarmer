0eb2b4b14ab90d539b51c52a5da2d739949126c2
Simple <69445064+Simple2006@users.noreply.github.com>
2021-02-13 14:43:49 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import multiprocessing
 
-for bot in ('dankmemerSend', 'dankmemerReact):
+for bot in ('dankmemerSend', 'dankmemerReact'):
     p = multiprocessing.Process(target=lambda: __import__(bot))
     p.start()
