1d80c4c79ba37cdda027e5e5e81d305d71a9d8a2
Simple <69445064+Simple2006@users.noreply.github.com>
2021-02-13 14:40:06 -0800

    Update dankmemerSend.py

--- dankmemerSend.py
+++ dankmemerSend.py
@@ -1,26 +1,28 @@
 import discord, time, os
+client = discord.Client()
 
-class MyClient(discord.Client):
+@client.event
+async def on_ready():
+    print("Logged on as {0}!".format(client.user.name))
 
-    async def on_ready(self):
-        print("Logged on as {0}!".format(self.user))
-        channel = client.get_channel(752051281950015570)
-        while True:
-            await channel.send("pls hunt")
-            time.sleep(5)
-            await channel.send("pls search")
-            time.sleep(5)
-            await channel.send("pls fish")
-            time.sleep(9)
-            await channel.send("pls beg")
-            time.sleep(20)
-            await channel.send("pls pm")
-            time.sleep(5)
-            await channel.send("pls beg")
-            time.sleep(5)
-            await channel.send("pls dep all")
-            time.sleep(15)
+@client.event
+async def on_connect():
+    channel = client.get_channel(752051281950015570)
+    while True:
+        await channel.send("pls hunt")
+        time.sleep(5)
+        await channel.send("pls search")
+        time.sleep(5)
+        await channel.send("pls fish")
+        time.sleep(9)
+        await channel.send("pls beg")
+        time.sleep(20)
+        await channel.send("pls pm")
+        time.sleep(5)
+        await channel.send("pls beg")
+        time.sleep(5)
+        await channel.send("pls dep all")
+        time.sleep(15)
             
-client = MyClient()
 token = os.getenv('DISCORD_BOT_TOKEN')
 client.run(token, bot = False)
